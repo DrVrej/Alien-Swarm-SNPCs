ba66fccda767e5415aeeec496e3c9d90f45b0c0f
DrVrej <DrVrej@users.noreply.github.com>
2025-02-18 01:53:09 -0500

    Cleanup entity spawn menu variables

--- lua/entities/sent_vj_as_dronesp.lua
+++ lua/entities/sent_vj_as_dronesp.lua
@@ -10,8 +10,6 @@ ENT.Type 			= "anim"
 ENT.PrintName 		= "Drone Spawner"
 ENT.Author 			= "DrVrej"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
-ENT.Purpose 		= "Spawn it and fight with it!"
-ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Alien Swarm"
 
 ENT.Spawnable		= false
